33d31a72a1b0b0cbf64a01b9e73b896e9fce700b
Mathias <depotter.mathias@hotmail.com>
2020-12-15 21:57:45 +0000

    Updating script for git

--- exec.sh
+++ exec.sh
@@ -2,3 +2,4 @@
 git -C /mnt/c/Users/mathias.depotter/Desktop/AoC2020/ add -A
 git -C /mnt/c/Users/mathias.depotter/Desktop/AoC2020/ commit -m "$1"
 git -C /mnt/c/Users/mathias.depotter/Desktop/AoC2020/ push origin master
+
