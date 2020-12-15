040211a3127cacac414192ce39eb1532cde57702
Mathias <depotter.mathias@hotmail.com>
2020-12-15 21:56:10 +0000

    testing

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 git -C /mnt/c/Users/mathias.depotter/Desktop/AoC2020/ add -A
 git -C /mnt/c/Users/mathias.depotter/Desktop/AoC2020/ commit -m "$1"
 git -C /mnt/c/Users/mathias.depotter/Desktop/AoC2020/ push origin master
